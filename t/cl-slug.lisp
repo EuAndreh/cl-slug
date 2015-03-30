366d8b8f087ec41e53a3771d9c2c253d4fcd500a
André Miranda <andremiramor@gmail.com>
2015-03-30 09:34:50 -0300

    working with IS-ERROR

--- t/cl-slug.lisp
+++ t/cl-slug.lisp
@@ -317,7 +317,7 @@
 
 (deftest invalid-charset-error-test
   (is-error (asciify "ASCII string" :jp)
-            'invalid-charset-error;;(or error simple-error)
+            (or simple-error)
             "INVALID-CHARSET-ERROR is thrown with SLUGIFY.")
   (is-error (asciify "ASCII string" :jp)
             (or error simple-error)
