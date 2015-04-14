2986de8c104c3e8b68ff42dadb7221cbe45a2891
André Miranda <andremiramor@gmail.com>
2015-04-14 20:51:38 -0300

    Made the accentuation removing algorithm faster and simpler

--- src/cl-slug.lisp
+++ src/cl-slug.lisp
@@ -9,11 +9,7 @@
   (:documentation "Main (and only) package. Package nickname SLUG also available."))
 (in-package cl-slug)
 
-(defmacro aif2 (test then &optional else)
-  `(multiple-value-bind (it win) ,test
-     (if win
-         ,then
-          ,else)))
+
 
 (defparameter *available-languages* ()
   "Alist with (KEY . LANGUAGE-NAMESTRING).")
@@ -28,96 +24,96 @@
 
 (defmacro add-language (name key-code accentuation-alist
                         &optional special-chars-alist)
-  (flet ((add-upcase-chars (alist)
-           "Adds an equivalent upcase character cons pair to every cons pair."
+  (flet ((add-downcase-equivalent (alist)
+           "Adds an equivalent downcase cons pair to every cons pair."
            (remove-duplicates
             (append alist (mapcar (lambda (pair)
-                                    (cons (char-upcase (car pair))
-                                          (char-upcase (cdr pair))))
-                                  alist))))
-         (add-upcase-string (alist)
-           "Adds an equivalent upcase string cons pair to every string cons pair that has a second element that changes from downcase to upcase."
-           (remove-duplicates
-            (append alist (mapcar (lambda (pair)
-                                    (cons (string-upcase (car pair))
-                                          (string-upcase (cdr pair))))
-                                  alist))
-             :test #'string= :key #'cdr :from-end t)))
+                                    (cons (string-downcase (car pair))
+                                          (string-downcase (cdr pair))))
+                                  alist)))))
     `(progn
        (pushnew (cons ,key-code ,name) *available-languages* :key #'car)
        ,@(mapcar (lambda (pair)
-                   `(setf (gethash ,(cdr pair) %accentuations) ,(car pair)))
-                 (add-upcase-chars accentuation-alist))
+                   `(setf (gethash ,(string (cdr pair)) %accentuations)
+                          ,(string (car pair))))
+                 (add-downcase-equivalent accentuation-alist))
        ,@(mapcar (lambda (pair)
-                   `(setf (gethash ,(cdr pair) %special-chars) ,(car pair)))
-                 (add-upcase-string special-chars-alist))
+                   `(setf (gethash ,(string (cdr pair)) %special-chars)
+                          ,(string (car pair))))
+                 (add-downcase-equivalent special-chars-alist))
         ,key-code)))
 
 (add-language "Dansk (Danish)" :da
-              ((#\e . #\è) (#\o . #\ò) (#\a . #\â)
-               (#\e . #\é) (#\o . #\ó)
-               (#\e . #\ê) (#\o . #\ô)
-                           (#\o . #\ø))
-              (("aa" . "å") ("ae" . "æ")))
+              ((e . è) (o . ò) (a . â)
+               (e . é) (o . ó)
+               (e . ê) (o . ô)
+                       (o . ø))
+              ((aa . å) (ae . æ)))
 
 (add-language "Deutsch (German)" :de
-              ((#\a . #\ä) (#\e . #\ë) (#\i . #\ï) (#\o . #\ö) (#\u . #\ü))
-              (("ss" . "ß")))
+              ((a . ä) (e . ë) (i . ï) (o . ö) (u . ü))
+              ((ss . ß)))
 
 (add-language "English" :en ())
 
 (add-language "Español (Spanish)" :es
-              ((#\a . #\á) (#\e . #\é) (#\i . #\í) (#\o . #\ó) (#\u . #\ú)
-               (#\n . #\ñ)             (#\i . #\ï)             (#\u . #\ü)))
+              ((a . á) (e . é) (i . í) (o . ó) (u . ú)
+               (n . ñ)         (i . ï)         (u . ü)))
 
 (add-language "Esperanto" :eo
-              ((#\c . #\ĉ) (#\g . #\ĝ) (#\h . #\ĥ) (#\j . #\ĵ) (#\s . #\ŝ)
-               (#\u . #\ŭ)))
+              ((c . ĉ) (g . ĝ) (h . ĥ) (j . ĵ) (s . ŝ)
+               (u . ŭ)))
 
 (add-language "Français (French)" :fr
-              ((#\a . #\â) (#\e . #\ê) (#\i . #\î) (#\o . #\ô) (#\u . #\û)
-               (#\a . #\à) (#\e . #\è)                         (#\u . #\ù)
-                           (#\e . #\é)
-                           (#\e . #\ë) (#\i . #\ï)             (#\u . #\ü)
-               (#\c . #\ç) (#\y . #\ÿ) (#\i . #\î))
-              (("oe" . "œ") ("ae" . "æ")))
+              ((a . â) (e . ê) (i . î) (o . ô) (u . û)
+               (a . à) (e . è)                 (u . ù)
+                       (e . é)
+                       (e . ë) (i . ï)         (u . ü)
+               (c . ç) (y . ÿ) (i . î))
+              ((oe . œ) (ae . æ)))
 
 (add-language "Italiano (Italian)" :it
-              ((#\e . #\è) (#\o . #\ò) (#\i . #\î)
-               (#\e . #\é) (#\o . #\ó)))
+              ((e . è) (o . ò) (i . î)
+               (e . é) (o . ó)))
 
 (add-language "Norsk (Norwegian)" :no
-              ((#\e . #\è) (#\o . #\ò) (#\a . #\â)
-               (#\e . #\é) (#\o . #\ó)
-               (#\e . #\ê) (#\o . #\ô)
-                           (#\o . #\ø))
-              (("aa" . "å") ("ae" . "æ")))
+              ((e . è) (o . ò) (a . â)
+               (e . é) (o . ó)
+               (e . ê) (o . ô)
+                       (o . ø))
+              ((aa . å) (ae . æ)))
 
 (add-language "Português (Portuguese)" :pt
-              ((#\a . #\á) (#\e . #\é) (#\i . #\í) (#\o . #\ó) (#\u . #\ú)
-               (#\a . #\â) (#\e . #\ê)             (#\o . #\ô) (#\u . #\ü)
-               (#\a . #\à)             (#\c . #\ç) (#\o . #\õ)
-               (#\a . #\ã)))
+              ((a . á) (e . é) (i . í) (o . ó) (u . ú)
+               (a . â) (e . ê)         (o . ô) (u . ü)
+               (a . à)         (c . ç) (o . õ)
+               (a . ã)))
 
 (add-language "Rumàntsch (Romansh)" :rm
-              ((#\e . #\é) (#\i . #\ï) (#\o . #\ö) (#\u . #\ü)
-               (#\e . #\è)
-               (#\e . #\ê)))
+              ((e . é) (i . ï) (o . ö) (u . ü)
+               (e . è)
+               (e . ê)))
 
 (add-language "Suomi (Finnish)" :fi
-              ((#\a . #\ä) (#\o . #\ö) (#\u . #\ü)))
+              ((a . ä) (o . ö) (u . ü)))
 
 (add-language "Svenska (Swedish)" :sv
-              ((#\a . #\ä) (#\o . #\ö) (#\u . #\ü))
-              (("aa" . "å")))
+              ((a . ä) (o . ö) (u . ü))
+              ((aa . å)))
 
 (defun remove-accentuation (string)
   "Removes accentuation (according to %ACCENTUATIONS) from STRING."
-  (map 'string (lambda (char)
-                 (aif2 (gethash char %accentuations)
-                       it
-                       char))
-       string))
+  (maphash (lambda (k v)
+             (setf string (ppcre:regex-replace-all k string v)))
+          %accentuations)
+  string)
+
+(defun remove-special-chars (string)
+  "Removes all special characters stored in %SPECIAL-CHARS using #'PPCRE:REPLACE-REGEX-ALL."
+  (maphash (lambda (k v)
+             (setf string (ppcre:regex-replace-all k string v)))
+           %special-chars)
+  string)
 
 (defun remove-ponctuation (string)
   "Removes ponctuation and other special characters from STRING according #'ALPHANUMERICP."
@@ -144,13 +140,6 @@
     (remove-repeated-separator
      (string-trim (list *slug-separator*)
                   (substitute-ponctuation-by-separator string)))))
-
-(defun remove-special-chars (string)
-  "Removes all special characters stored in %SPECIAL-CHARS using #'PPCRE:REPLACE-REGEX-ALL."
-  (maphash (lambda (k v)
-             (setf string (ppcre:regex-replace-all k string v)))
-           %special-chars)
-  string)
 
 (defun asciify (string)
   "Removes the accentuation and ponctuation of the given STRING."
